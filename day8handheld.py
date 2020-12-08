867bbe9d349c294cbaf54210bd9551a4279a62b9
Zane Othman <zane.othman@gmail.com>
2020-12-07 22:15:56 -0800

    Added day 8 part 2 solution
    Also removed newline from the end of day 8's input, as it was causing
    problems.

--- day8handheld.py
+++ day8handheld.py
@@ -1,13 +1,23 @@
-def execute_stopping_at_dupe(instructions, visited, cursor, acc):
-    """
-    Executes instructions, stopping when the current instruction has been
-    executed previously.
+from copy import deepcopy
+from functools import partial
 
-    Returns the value in the accumulator at this time.
+
+def execute_handheld_code(instructions, visited, cursor, acc):
     """
+    Executes the supplied instructions, returning the final value of `acc`.
+
+    If the instructions terminated normally (i.e. reached the end of the input),
+    `True` will be returned along with the value of `acc`; otherwise, `False` will
+    be returned
+    """
+    # Check if at end of instructions
+    if cursor == len(instructions):
+        print(acc, "is valid")
+        return acc, True
+
     # Check if current instruction has been executed before
     if cursor in visited:
-        return acc
+        return acc, False
     else:
         visited.add(cursor)
 
@@ -16,16 +26,32 @@ def execute_stopping_at_dupe(instructions, visited, cursor, acc):
 
     # Increment accumulator
     if op == "acc":
-        return execute_stopping_at_dupe(instructions, visited, cursor + 1,
-                                        acc + int(value))
+        return execute_handheld_code(instructions, visited, cursor + 1,
+                                     acc + int(value))
     # Jump to instruction based on offset
     elif op == "jmp":
-        return execute_stopping_at_dupe(instructions, visited,
-                                        cursor + int(value), acc)
+        return execute_handheld_code(instructions, visited,
+                                     cursor + int(value), acc)
 
     # Noop
     elif op == "nop":
-        return execute_stopping_at_dupe(instructions, visited, cursor + 1, acc)
+        return execute_handheld_code(instructions, visited, cursor + 1, acc)
+
+
+def swap_instruction(instruction_list, idx):
+    """
+    Swaps a `jmp` instruction with a `nop` or vice versa.
+    """
+    instructions_copy = deepcopy(instruction_list)
+
+    current_instruction, _ = instructions_copy[idx]
+
+    if current_instruction == "jmp":
+        instructions_copy[idx][0] = "nop"
+    elif current_instruction == "nop":
+        instructions_copy[idx][0] = "jmp"
+
+    return instructions_copy
 
 
 if __name__ == "__main__":
@@ -33,8 +59,33 @@ if __name__ == "__main__":
         raw_input = f.read().split("\n")
 
     instruction_pairs = list(map(lambda line: line.split(" "), raw_input))
-    acc_before_repeat = execute_stopping_at_dupe(instruction_pairs, set(), 0,
+
+    # Part 1 (accumulator before repeating)
+    acc_before_repeat, _ = execute_handheld_code(instruction_pairs, set(), 0,
                                                  0)
 
     print(f"Part 1 result (accumulator before repeating): {acc_before_repeat}")
     assert acc_before_repeat == 1816, f"Unexpected part 1 result: {acc_before_repeat}"
+
+    # Part 2 (accumulator after exiting normally)
+
+    # Replacing `nop` with `jmp`
+    nop_lines = filter(lambda line: line[0] == "nop", instruction_pairs)
+    nop_indices = map(instruction_pairs.index, nop_lines)
+    nop_swap_results = map(
+        lambda updated: execute_handheld_code(updated, set(), 0, 0),
+        map(partial(swap_instruction, instruction_pairs), nop_indices))
+    possible_result = list(filter(lambda res: res[1], nop_swap_results))
+
+    # Replacing `jmp` with `nop`, if necessary
+    if not possible_result:
+        jmp_lines = filter(lambda line: line[0] == "jmp", instruction_pairs)
+        jmp_indices = map(instruction_pairs.index, jmp_lines)
+        jmp_swap_results = map(
+            lambda updated: execute_handheld_code(updated, set(), 0, 0),
+            map(partial(swap_instruction, instruction_pairs), jmp_indices))
+        possible_result = list(filter(lambda res: res[1], jmp_swap_results))
+
+    final_acc, _ = possible_result[0]
+    print(f"Part 2 result (accumulator after normal exit): {final_acc}")
+    assert final_acc == 1149, f"Unexpected part 2 result: {final_acc}"
